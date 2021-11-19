e7d7e3838e51bfebe1eeb164489e93d9b4f1fc40
Johanna Jimeno <johannajimenoo@gmail.com>
2021-11-19 20:07:07 +0800

    Updated sql_commands.py

--- Objects/sql_commands.py
+++ Objects/sql_commands.py
@@ -1,11 +1,12 @@
 import mysql.connector
+import os
 
 # Database connection
 mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="ict2103project_database"
+    host="172.24.117.56",
+    user = "monty",
+    password = "Password123!",
+    database="SIT"
 )
 
 # Things to do: create functions for the following SQL statements
@@ -28,6 +29,8 @@ mydb = mysql.connector.connect(
 #     print("{} database created successfully.".format(db_name))
 
 
+# Retrieves data from all the columns from "table_name"
+# SQL = SELECT * FROM "table_name"
 def select_all_columns(table_name):
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM {}".format(table_name))
@@ -38,6 +41,8 @@ def select_all_columns(table_name):
     return myresult
 
 
+# Retrieves data from specified columns "column_names" from "table_name"
+# SQL = SELECT "column_names" FROM "table_name"
 def select_certain_columns(table_name, column_names):
     mycursor = mydb.cursor()
     mycursor.execute("SELECT {} FROM {}".format(column_names, table_name))
@@ -48,6 +53,8 @@ def select_certain_columns(table_name, column_names):
     return myresult
 
 
+# Updates "table_name" by setting the "column_name" = "value" where the "identifier" = "identifier_value"
+# SQL = UPDATE "table_name" SET "column_name" = "value" WHERE "identifier" = "identifier_value"
 def update_data(table_name, identifier, identifier_value, column_name, value):
     mycursor = mydb.cursor()
     mycursor.execute("UPDATE {} SET {} = '{}' WHERE {} = '{}'".format(
@@ -58,6 +65,8 @@ def update_data(table_name, identifier, identifier_value, column_name, value):
     print("{} table updated successfully.".format(table_name))
 
 
+# Deletes the row from "table_name" where the "identifier" = "identifier_value"
+# SQL = DELETE FROM "table_name" WHERE "identifier" = "identifier_value"
 def delete_data(table_name, identifier, identifier_value):
     mycursor = mydb.cursor()
 
@@ -75,6 +84,8 @@ def delete_data(table_name, identifier, identifier_value):
     print("data deleted from {} table successfully.".format(table_name))
 
 
+# Deletes all data in "table_name"
+# SQL = DELETE FROM "table_name"
 def delete_all(table_name):
     mycursor = mydb.cursor()
     mycursor.execute("DELETE FROM {}".format(table_name))
@@ -86,6 +97,7 @@ def delete_all(table_name):
     print("deleted all data from {} table successfully".format(table_name))
 
 
+# Resets auto-increment index of "table_name" *Don't use (mainly for delete_all function)
 def reset_index(table_name):
     mycursor = mydb.cursor()
     mycursor.execute("ALTER TABLE {} AUTO_INCREMENT = 1".format(table_name))
@@ -93,29 +105,53 @@ def reset_index(table_name):
     mydb.commit()
 
 
-def insert_data(table_name, val1, val2):
-    mycursor = mydb.cursor()
-    mycursor.execute("INSERT INTO test VALUES ('', %s, %s)", (val1, val2))
+# Inserts data values into "table_name"
+# SQL = INSERT INTO "table_name" (columns) VALUES (values)
+# data is a dictionary for e.g.
+# data = {
+#     "name": "William",
+#     "desc": "person3"
+# }
+def insert_data(table_name, data):
+    columns = []
+    values = []
 
+    for key, value in data.items():
+        columns.append(key)
+        values.append(value)
+
+    columns_string = ', '.join(f"`{w}`" for w in columns)
+    values_string = ', '.join(f"'{w}'" for w in values)
+
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO " + table_name + \
+        " (" + columns_string + ") VALUES (" + values_string + ")"
+
+    mycursor.execute(sql)
     mydb.commit()
 
     print("data inserted to {} table successfully.".format(table_name))
+
 
 # Test Data
 # print(select_all_columns("test"))
 # print("\n")
 # print(select_certain_columns("test", "id, name"))
 # print("\n")
-# update_data("test", "id", "1", "name", "Johanna")
+# update_data("test", "id", "1", "name", "Bianca")
 # print("\n")
 # print(select_all_columns("test"))
 
-# Iterate through dictionary to insert into table
-# people = {"Andrea": "person1", "Miguel": "person2", "Grace": "person3"}
-# for key, value in people.items():
-#     insert_data("test", key, value)
 
-# Delete commands
+# data = {
+#     "name": "William",
+#     "desc": "person3"
+# }
+
+# Insert
+# insert_data("test", data)
+
+# Delete
 # print("\n")
-# delete_data("test", "id", "1")
+# delete_data("test", "id", "5")
 # delete_all("test")
