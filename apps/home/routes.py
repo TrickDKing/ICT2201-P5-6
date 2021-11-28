4b22ba039d5a3b83588a2f06c0ab4c56b0c3bb8c
Carlton <carltonfoo@gmail.com>
2021-11-28 21:47:50 +0800

    completed edit profile functionality, and added more robust dbfuncs

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -2,8 +2,28 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+from flask_login import (
+    current_user,
+    login_required
+)
+from apps.home.dbfuncs import select_data, update_data
+from apps.authentication.models import Users
+from apps.authentication.util import hash_pass
 
-from apps.home.dbfuncs import select_all_columns
+# Helper - Extract current page name from request
+def get_segment(request):
+
+    try:
+
+        segment = request.path.split('/')[-1]
+
+        if segment == '':
+            segment = 'index'
+
+        return segment
+
+    except:
+        return None
 
 @blueprint.route('/index')
 @login_required
@@ -22,13 +42,13 @@ def route_template(template):
             template += '.html'
             
         if template == 'profile.html':
-            data = select_all_columns("users")
+            data = select_data(table_name="users", filterBy=['username'], filterVal=[str(current_user)])
 
         # Detect the current page
         segment = get_segment(request)
 
         # Serve the file (if exists) from app/templates/home/FILE.html
-        return render_template("home/" + template, segment=segment)
+        return render_template("home/" + template, segment=segment, data=data)
 
     except TemplateNotFound:
         return render_template('home/page-404.html'), 404
@@ -37,17 +57,51 @@ def route_template(template):
         return render_template('home/page-500.html'), 500
 
 
-# Helper - Extract current page name from request
-def get_segment(request):
 
-    try:
+@blueprint.route('/updateprofile', methods=['POST'])
+@login_required
+def saveDetails():
+    print("Updating profile...")
+    if request.method == 'POST':
 
-        segment = request.path.split('/')[-1]
+        userexist = Users.query.filter_by(username=request.form['username']).first()
 
-        if segment == '':
-            segment = 'index'
+        data = {
+            "name": str(request.form['name'])
+        }
 
-        return segment
+        if request.form['username'] == str(current_user):
 
-    except:
-        return None
+            if request.form['password']:
+                password = str(request.form['password'])
+                passhash = str(hash_pass(password)).split("'")[1]
+
+                data["password"] = passhash
+
+            update_data(table_name="users", data=data, identifier="username", identifier_value=str(current_user))
+            
+            result = "Profile updated successfully!"
+
+        elif not userexist:
+
+            data["username"] = str(request.form['username'])
+
+            if request.form['password']:
+                password = str(request.form['password'])
+                passhash = str(hash_pass(password)).split("'")[1]
+
+                data["password"] = passhash
+
+            update_data(table_name="users", data=data, identifier="username", identifier_value=str(current_user))
+            
+            result = "Profile updated successfully!"
+
+            data = select_data(table_name="users", filterBy=['username'], filterVal=[str(request.form['username'])])
+            return render_template('home/profile.html', data=data)
+
+        else:
+            result = "Username exists"
+
+        print(result)
+        data = select_data(table_name="users", filterBy=['username'], filterVal=[str(current_user)])
+        return render_template('home/profile.html', data=data)
