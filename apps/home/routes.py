9c4e40bf060f1041bbcab5d53c897d025d5e7888
IVIillionaire <72209857+IVIillionaire@users.noreply.github.com>
2021-12-06 15:53:43 +0800

    Revert "Level Select update"
    This reverts commit d36f957ae3efadf92037c39caf0f3075f4bd3140.

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -9,12 +9,8 @@ from flask_login import (
 from apps.home.dbfuncs import select_data, update_data, select_all_columns_with_condition, get_best_score_by_level
 from apps.authentication.models import Users
 from apps.authentication.util import hash_pass
-from operator import itemgetter
-from itertools import groupby
 
 # Helper - Extract current page name from request
-
-
 def get_segment(request):
 
     try:
@@ -28,7 +24,6 @@ def get_segment(request):
 
     except:
         return None
-
 
 @blueprint.route('/index')
 @login_required
@@ -45,43 +40,31 @@ def route_template(template):
 
         if not template.endswith('.html'):
             template += '.html'
-
+            
         if template == 'profile.html':
-            data = select_data(table_name="users", filterBy=[
-                               'username'], filterVal=[str(current_user)])
-
-        elif template == 'scoreboard.html':  # MUST INCLUDE THIS TO WORK, BECAUSE DATA IS ABSENT EN DING PART
+            data = select_data(table_name="users", filterBy=['username'], filterVal=[str(current_user)])
+        
+        elif template == 'scoreboard.html':                    #MUST INCLUDE THIS TO WORK, BECAUSE DATA IS ABSENT EN DING PART
             #data = select_all_columns_with_condition("highScore","totalScore")
-            data = select_all_columns_with_condition("attempts", "score")
+            data = select_all_columns_with_condition("attempts","score")
             segment = get_segment(request)
             return render_template("home/" + template, segment=segment, data=data)
 
         elif template == 'instructions.html':
-            data = select_data(table_name="users", filterBy=[
-                               'username'], filterVal=[str(current_user)])
-            # Detect the current page
-            segment = get_segment(request)
-            # Serve the file (if exists) from app/templates/home/FILE.html
-            return render_template("home/" + template, segment=segment, data=data)
+             data = select_data(table_name="users", filterBy=['username'], filterVal=[str(current_user)])
+             # Detect the current page
+             segment = get_segment(request)
+             # Serve the file (if exists) from app/templates/home/FILE.html
+             return render_template("home/" + template, segment=segment, data=data)
+        
 
-        elif template == 'levelselect.html':
+        elif template == 'levelselect.html':                    
             data = get_best_score_by_level("attempts","level_id","level_id")
-            attempts = select_data(table_name="attempts")
-            # print(attempts)
-            # attempts = sorted(attempts, key=itemgetter('level_id'))
-            # Display data grouped by grade
-            # level_attempts = {"1": [],"2": [],"3": [],"4": [],"5": [], "6": [],"7": [],"8": [],"9": []}
-            # for key, value in groupby(attempts, key=itemgetter('level_id')):
-            #     print(key)
-            #     for k in value:
-            #         print(k)
             segment = get_segment(request)
             return render_template("home/" + template, segment=segment, data=data)
-        # SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
-        # Whenever the groupby or complicated sql statements fail, execute this to allow nonaggregate queries
 
-        elif template == 'attempthistory.html':
-            data = select_all_columns_with_condition("attempts", "attempt_id")
+        elif template == 'attempthistory.html':                    
+            data = select_all_columns_with_condition("attempts","attempt_id")
             segment = get_segment(request)
             return render_template("home/" + template, segment=segment, data=data)
 
@@ -92,14 +75,14 @@ def route_template(template):
         return render_template('home/page-500.html'), 500
 
 
+
 @blueprint.route('/updateprofile', methods=['POST'])
 @login_required
 def saveDetails():
     print("Updating profile...")
     if request.method == 'POST':
 
-        userexist = Users.query.filter_by(
-            username=request.form['username']).first()
+        userexist = Users.query.filter_by(username=request.form['username']).first()
 
         data = {
             "name": str(request.form['name'])
@@ -113,9 +96,8 @@ def saveDetails():
 
                 data["password"] = passhash
 
-            update_data(table_name="users", data=data,
-                        identifier="username", identifier_value=str(current_user))
-
+            update_data(table_name="users", data=data, identifier="username", identifier_value=str(current_user))
+            
             result = "Profile updated successfully!"
 
         elif not userexist:
@@ -128,43 +110,16 @@ def saveDetails():
 
                 data["password"] = passhash
 
-            update_data(table_name="users", data=data,
-                        identifier="username", identifier_value=str(current_user))
-
+            update_data(table_name="users", data=data, identifier="username", identifier_value=str(current_user))
+            
             result = "Profile updated successfully!"
 
-            data = select_data(table_name="users", filterBy=['username'], filterVal=[
-                               str(request.form['username'])])
+            data = select_data(table_name="users", filterBy=['username'], filterVal=[str(request.form['username'])])
             return render_template('home/profile.html', data=data)
 
         else:
             result = "Username exists"
 
         print(result)
-        data = select_data(table_name="users", filterBy=[
-                           'username'], filterVal=[str(current_user)])
+        data = select_data(table_name="users", filterBy=['username'], filterVal=[str(current_user)])
         return render_template('home/profile.html', data=data)
-
-
-@blueprint.route('/settings', methods=['GET', 'POST'])
-@login_required
-def saveSettings():
-    # speed = request.form['speed']
-    print("Updating profile...")
-    if request.method == 'POST':
-        data = {
-            "speed": request.form['speed']
-        }
-        print(data)
-        if request.form['speed']:
-            update_data(table_name="Settings", data=data,
-                        identifier="settings_id", identifier_value="1")
-
-            result = "Speed updated successfully!"
-        else:
-            result = "Updating failed"
-
-        return render_template('home/settings.html')
-    else:
-        settings = select_data(table_name="Settings")
-        return render_template('home/settings.html', speed=settings[0]["speed"])
